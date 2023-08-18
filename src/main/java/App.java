62946ca87d8d905106444091cc7c29e8a789f7c8
Chinaski00 <kost.ivanov.00@gmail.com>
2023-08-18 21:32:00 +0700

    PP 2.1.2 Задача

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,20 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
         System.out.println(bean.getMessage());
+        System.out.println(bean2.getMessage());
+
+
+        Cat cat = applicationContext.getBean(Cat.class);
+        Cat cat2 = applicationContext.getBean(Cat.class);
+
+
+        // Сравнение по ссылке
+        boolean areEqual = cat == cat2;
+
+        System.out.println("Beans are equal: " + areEqual);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,22 @@
+
+
+public class Cat {
+    private String name;
+    private int age;
+
+    public Cat() {
+    }
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
